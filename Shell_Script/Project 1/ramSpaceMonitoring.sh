2e7eb4f1795060e1807116e8ce9704d410a5733e
~Srikant <163234695+srikxcipher@users.noreply.github.com>
2024-07-30 11:28:45 +0530

    Added main code

--- Shell_Script/Project 1/ramSpaceMonitoring.sh
+++ Shell_Script/Project 1/ramSpaceMonitoring.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+
+FREE_SPACE=$(free -mt | grep "Total" | awk '{print $4}')
+TH=500
+if [[ $FREE_SPACE -lt $TH ]]
+then
+        echo "Warning, RAM is running low"
+else
+        echo "RAM Space is sufficient - $FREE_SPACE M"
+fi
 
